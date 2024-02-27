fbeb74e29e14139bc7d5072bbdf5ef4196e03022
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 12:36:53 -0800

    Greetings
    Typescript 45Q assignment exercise 12

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = ['Sanober', 'Faryal', 'Farah', 'Rafia'];
+// loop throufh each name in the array
+for (let name of names) {
+    // print a personalize message for each friend
+    console.log(`Assalam-o-Alaikum ${name}, I hope you are having a fantastic day!`);
+}
